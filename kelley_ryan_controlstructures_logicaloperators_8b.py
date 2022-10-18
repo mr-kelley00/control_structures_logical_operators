574fedf5eac9b6000a0e347cf9076c541a961582
Kelley <kelleyr@duvalschools.org>
2022-10-18 14:04:40 -0400

    v0.1
    Declared and defined backColor, cardValue, and cardSuit variables.

---
+++ kelley_ryan_controlstructures_logicaloperators_8b.py
@@ -0,0 +1,7 @@
+# Control Structures and Logical Operators, ryan Kelley, 10/18/22, 1:50pm, v0.1
+
+# Card Variables 
+backColor = ""
+cardValue = 0 # 2-10, Jack = 11, Queen = 12, King = 13, Ace = 14
+cardSuit = 0 # 1 = Clubs, 2 = Spades, 3 = Hearts, 4 = Diamonds 
+
