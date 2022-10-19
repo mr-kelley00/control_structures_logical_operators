72a81b09cb66c046e52e0a31570d726009adc5ee
Kelley <kelleyr@duvalschools.org>
2022-10-19 14:48:21 -0400

    Update Program.cs

--- C#/4A/Program.cs
+++ C#/4A/Program.cs
@@ -10,7 +10,21 @@ namespace ControlStructuresLogicalOperators
             // Card Attribute Values
             int cardValue; 
             string cardSuit; 
-            bool backIsBlue; 
+            int backIsBlue; 
+
+            // Collect User Input 
+            Console.WriteLine("Please enter the card value, 2-10, Jack = 11, Queen = 12, King = 13, Ace = 14.");
+            cardValue = Convert.ToInt32(Console.ReadLine());
+            
+            Console.WriteLine("Please enter the card suit: Clubs, Diamonds, Hearts, or Spades.");
+            cardSuit = System.Console.ReadLine(); 
+            
+            Console.WriteLine("Is the back of the card blue?  If yes, enter 1.  If no, enter 0.");
+            backIsBlue = Convert.ToInt32(Console.ReadLine());
+            
+            Console.WriteLine("Your card is " + cardSuit + " and has a value of " + cardValue + "and the back color value is" + backIsBlue + ".");
+            
+
 
         }
     }
