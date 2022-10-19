c944bd61cf39fb95f93c196d574f04fbccfb342a
Kelley <kelleyr@duvalschools.org>
2022-10-19 13:45:07 -0400

    Project

--- Python/kelley_ryan_controlstructures_logicaloperators.py
+++ Python/kelley_ryan_controlstructures_logicaloperators.py
@@ -1,7 +1,14 @@
-#Control Structures and Logical Operators, Ryan Kelley, 10/19/22, 12:50PM, v0.1
+#Control Structures and Logical Operators, Ryan Kelley, 10/19/22, 12:50PM, v0.2
 
 # Card Attributes 
 cardSuit = 0 # 1 = Clubs, 2 = Spades, 3 = Hearts, 4 = Diamonds 
 cardValue = 0 # 2-10, Jack = 11, Queen = 12, King = 13, Ace = 14
 cardBack = "" # Red or Blue 
 
+# Collect User Input 
+cardSuit = int(input("Please enter the card suit, 1 = Clubs, 2 = Spades, 3 = Hearts, 4 = Diamonds.\n"))
+cardValue = int(input("Please enter the card value, 2-10, Jack = 11, Queen = 12, King = 13, Ace = 14.\n"))
+cardBack = input("Please enter the color of the card back, either Red or Blue.\n")
+
+# Validate User Input 
+print(f"Your card suit is {cardSuit}, it has a value of {cardValue} and the back is {cardBack}.\n")
