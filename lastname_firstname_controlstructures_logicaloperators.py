673e9ccbceb81b31a87e188a0e274fb3cf33f41d
Ryan Kelley <kelleyr@duvalschools.org>
2022-10-24 10:12:32 -0400

    v0.1
    Declared and defined three variables for playing cards: backColor, cardValue, and cardSuit.

--- lastname_firstname_controlstructures_logicaloperators.py
+++ lastname_firstname_controlstructures_logicaloperators.py
@@ -1,1 +1,6 @@
-# Control Structures and Logical Operators, Ryan Kelley, 10/24/22, 10:07AM, v0.0 
+# Control Structures and Logical Operators, Ryan Kelley, 10/24/22, 10:07AM, v0.1 
+
+# Card Variables 
+backColor = 0 # 1 = Red, 2 = Blue 
+cardValue = 0 # 2-10, Jack = 11, Queen = 12, King = 13, Ace = 14
+cardSuit = 0 # 1 = Club, 2 = Spade, 3 = Heart, 4 = Diamond 
