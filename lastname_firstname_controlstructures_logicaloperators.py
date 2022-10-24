cef75fe313339a57c65ef8f115e9c97927e7c37e
Ryan Kelley <kelleyr@duvalschools.org>
2022-10-24 10:29:11 -0400

    v0.3
    Validated user input.

--- lastname_firstname_controlstructures_logicaloperators.py
+++ lastname_firstname_controlstructures_logicaloperators.py
@@ -1,4 +1,4 @@
-# Control Structures and Logical Operators, Ryan Kelley, 10/24/22, 10:23AM, v0.2 
+# Control Structures and Logical Operators, Ryan Kelley, 10/24/22, 10:28AM, v0.3 
 
 # Card Variables 
 backColor = 0 # 1 = Red, 2 = Blue 
@@ -10,3 +10,6 @@ cardSuit = 0 # 1 = Club, 2 = Spade, 3 = Heart, 4 = Diamond
 backColor = int(input("Please enter the card back, 1 for Red and 2 for Blue.\n")) 
 cardValue = int(input("Please enter 2-10, Jack = 11, Queen = 12, King = 13, Ace = 14.\n")) 
 cardSuit = int(input("Please enter the card suit. 1 = Club, 2 = Spade, 3 = Heart, 4 = Diamond \n")) 
+
+# Validate User Input -- Is the information being saved as expected? 
+print(f"Your card back color is {backColor}, it has a value of {cardValue} and the suit value is {cardSuit}.\n")
