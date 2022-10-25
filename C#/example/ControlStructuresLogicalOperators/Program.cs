9ec1944fb5893963f999319b1d7a302cba7fc6a2
Ryan Kelley <kelleyr@duvalschools.org>
2022-10-25 10:11:21 -0400

    v0.1
    Declared and defined variables to represent the card information.

--- C#/example/ControlStructuresLogicalOperators/Program.cs
+++ C#/example/ControlStructuresLogicalOperators/Program.cs
@@ -1,4 +1,4 @@
-﻿// Control Structures and Logical Operators, Ryan Kelley, 10/25/22, 10:02AM, v0.0 
+﻿// Control Structures and Logical Operators, Ryan Kelley, 10/25/22, 10:10AM, v0.1 
 using System;
 
 namespace ControlStructuresLogicalOperators
@@ -7,7 +7,12 @@ namespace ControlStructuresLogicalOperators
     {
         static void Main(string[] args)
         {
-            
+           // Declare and Define Variables and Data Types 
+           // C# Variables use this format: datatype variablename; 
+           int cardBack = 0; // 1 = Red, 2 = Blue  
+           int cardValue = 0; // 2-10, Jack = 11, Queen = 12, King = 13, Ace = 14
+           int cardSuit = 0; // 1 = Clubs, 2 = Spades, 3 = Hearts, 4 = Diamonds 
+
         }
     }
 }
