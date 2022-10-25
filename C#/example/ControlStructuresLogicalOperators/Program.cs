77f3c3fa66634c85d7a4d63927b459c7475675ce
Ryan Kelley <kelleyr@duvalschools.org>
2022-10-25 10:51:31 -0400

    v0.4
    Added examples of if Statements.

--- C#/example/ControlStructuresLogicalOperators/Program.cs
+++ C#/example/ControlStructuresLogicalOperators/Program.cs
@@ -1,4 +1,4 @@
-﻿// Control Structures and Logical Operators, Ryan Kelley, 10/25/22, 10:36AM, v0.3
+﻿// Control Structures and Logical Operators, Ryan Kelley, 10/25/22, 10:51AM, v0.4
 using System;
 
 namespace ControlStructuresLogicalOperators
@@ -24,10 +24,26 @@ namespace ControlStructuresLogicalOperators
             cardSuit = Convert.ToInt32(Console.ReadLine()); // By default, Console.ReadLine() stores as a string.  
 
             // Validate User Input -- Make sure the user is typing is being saved correctly.  
-            Console.WriteLine("Your card back is " + cardBack + ". It has a value of " + cardValue + " and the suit number is " + cardSuit + ".");
+            // Console.WriteLine("Your card back is " + cardBack + ". It has a value of " + cardValue + " and the suit number is " + cardSuit + ".");
             Console.WriteLine(string.Format("Your card back is {0}. It has a value of {1} and the suit number is {2}.", cardBack, cardValue, cardSuit));
 
+            // Control Structures -- Allow the program to make decisions or change flow / sequence of events. 
+            // if Statement 
+            if (cardValue > 10) // (cardValue > 10) is a CONDITIONAL EXPRESSION -- Is it True or False? 
+            {
+                Console.WriteLine("You have a face card, either a Jack, Queen, King, or Ace!");                
+            }
 
+            if (cardSuit == 1)
+            {
+                Console.WriteLine("You have a Club.");                   
+            }
+            
+            // Pause the video, write your own if statement using the cardBack variable. 
+            if (cardBack == 1)
+            {
+                Console.WriteLine("You have a card with a red back.");                   
+            }
            
         }
     }
