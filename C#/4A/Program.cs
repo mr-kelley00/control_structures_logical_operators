1b1e813a9aa86fb5354a37bb08e1b1aae5f62a30
Kelley <kelleyr@duvalschools.org>
2022-10-26 08:12:09 -0400

    Test

--- C#/4A/Program.cs
+++ C#/4A/Program.cs
@@ -8,22 +8,22 @@ namespace ControlStructuresLogicalOperators
         static void Main(string[] args)
         {
             // Card Attribute Values
-            int cardValue; 
-            string cardSuit; 
-            int backIsBlue; 
+            int cardValue = 0; // 2-10, Jack = 11, Queen = 12, King = 13, Ace = 14
+            int cardSuit = 0; // 1 = Clubs, 2 = Spades, 3 = Hearts, 4 = Diamonds
+            int cardBack = 0; // 1 = Red, 2 = Blue 
 
             // Collect User Input 
             Console.WriteLine("Please enter the card value, 2-10, Jack = 11, Queen = 12, King = 13, Ace = 14.");
             cardValue = Convert.ToInt32(Console.ReadLine());
             
             Console.WriteLine("Please enter the card suit: Clubs, Diamonds, Hearts, or Spades.");
-            cardSuit = System.Console.ReadLine(); 
+            cardSuit = Convert.ToInt32(Console.ReadLine());
             
             Console.WriteLine("Is the back of the card blue?  If yes, enter 1.  If no, enter 0.");
-            backIsBlue = Convert.ToInt32(Console.ReadLine());
+            cardBack = Convert.ToInt32(Console.ReadLine());
             
-            Console.WriteLine("Your card is " + cardSuit + " and has a value of " + cardValue + "and the back color value is" + backIsBlue + ".");
-            Console.WriteLine(string.Format("Your card back is {0}. It has a value of {1} and the suit value is {2}.", backIsBlue, cardValue, cardSuit));
+            Console.WriteLine("Your card is " + cardSuit + " and has a value of " + cardValue + "and the back color value is" + cardBack + ".");
+            Console.WriteLine(string.Format("Your card back is {0}. It has a value of {1} and the suit value is {2}.", cardBack, cardValue, cardSuit));
 
 
         }
