6c001b8fbf4ffeb6337d341abe75f412fdf8e51d
Ryan Kelley <kelleyr@duvalschools.org>
2022-10-27 15:48:10 -0400

    v1.0a
    Style change, { after conditionals instead of separate line.

--- C#/example/ControlStructuresLogicalOperators/Program.cs
+++ C#/example/ControlStructuresLogicalOperators/Program.cs
@@ -1,4 +1,4 @@
-﻿// Control Structures and Logical Operators, Ryan Kelley, 10/27/22, 12:32PM, v1.0
+﻿// Control Structures and Logical Operators, Ryan Kelley, 10/27/22, 3:37PM, v1.0a
 using System;
 
 namespace ControlStructuresLogicalOperators
@@ -30,99 +30,80 @@ namespace ControlStructuresLogicalOperators
             // Control Structures -- Allow the program to make decisions or change flow / sequence of events. 
             // if Statement 
             /*
-            if (cardValue > 10) // (cardValue > 10) is a CONDITIONAL EXPRESSION -- Is it True or False? 
-            {
+            if (cardValue > 10) // (cardValue > 10) { // is a CONDITIONAL EXPRESSION -- Is it True or False? 
                 Console.WriteLine("You have a face card, either a Jack, Queen, King, or Ace!");                
             }
 
-            if (cardSuit == 1)
-            {
+            if (cardSuit == 1) {
                 Console.WriteLine("You have a Club.");                   
             }
             
             // Pause the video, write your own if statement using the cardBack variable. 
-            if (cardBack == 1)
-            {
+            if (cardBack == 1) {
                 Console.WriteLine("You have a card with a red back.");                   
             }
             */ 
 
             /*
             // if-else Statement 
-            if (cardValue < 11)
-            {
+            if (cardValue < 11) {
                 Console.WriteLine("You have a card that is not a face card.");                      
             }
-            else
-            {
+            else {
                 Console.WriteLine("You have a card that is a face card.");                      
             }
 
-            if (cardBack == 1)
-            {
+            if (cardBack == 1) {
                 Console.WriteLine("You have a card with a red back.");                   
             }
-            else 
-            {
+            else {
                 Console.WriteLine("You have a card with a blue back.");                   
             }
 
             // Pause the video, write an if-else statement using the cardSuit variable.  
-            if (cardSuit == 1)
-            {
+            if (cardSuit == 1) {
                 Console.WriteLine("You have a card that is a Clubs.");                   
             }
-            else
-            {
+            else {
                 Console.WriteLine("You have a card that is a Hearts, Diamonds, or Spades.");                   
             }
             */ 
 
             // if - else if - else 
             /*
-             if (cardSuit == 1)
-            {
+             if (cardSuit == 1) {
                 Console.WriteLine("You have a card that is a Clubs.");                   
             }
-            else if (cardSuit == 2)
-            {
+            else if (cardSuit == 2) {
                 Console.WriteLine("You have a card that is a Spades.");                   
             }
-            else if (cardSuit == 3)
-            {
+            else if (cardSuit == 3) {
                 Console.WriteLine("You have a card that is a Hearts.");                   
             }
-            else if (cardSuit == 4)
-            {
+            else if (cardSuit == 4) {
                 Console.WriteLine("You have a card that is a Diamonds.");                   
             }                  
-            else
-            {
+            else {
                 Console.WriteLine("Your card suit was not properly detected, please restart the program and try again.");                   
             }
+            
             // Pause the video, create an if-else if-else block using the Card Value.  Print exactly which face card, or print 2-10, or print an error. 
-            if (cardValue == 14)
-            {
+            if (cardValue == 14) {
                 Console.WriteLine("You have a card that is an Ace card.");                      
             }
-            else if (cardValue == 13)
-            {
+            else if (cardValue == 13) {
                 Console.WriteLine("You have a card that is a King card.");                      
             }
-            else if (cardValue == 12)
-            {
+            else if (cardValue == 12) {
                 Console.WriteLine("You have a card that is a Queen card.");                      
             }
-            else if (cardValue == 11)
-            {
+            else if (cardValue == 11) {
                 Console.WriteLine("You have a card that is a Jack card.");                      
             }
-            else if (cardValue < 11)
-            {
+            else if (cardValue < 11) {
                 Console.WriteLine("You have a card that is a 2-10.");                      
             }
-            else
-            {
+            else {
                 Console.WriteLine("Your card value was not saved correctly.");                      
             }
             */ 
@@ -130,53 +111,42 @@ namespace ControlStructuresLogicalOperators
             // Logical Operators -- and or not 
             // AND && 
             /*
-            if (cardValue > 10 && cardSuit == 1) // ALL conditions must be true for the statement to be true. 
-            {
+            if (cardValue > 10 && cardSuit == 1) { // ALL conditions must be true for the statement to be true. 
                 Console.WriteLine("You have a face card that is a Club.");
             }
-            else
-            {
+            else {
                 Console.WriteLine("You do not have a face card that is a Club.");
             }
 
             // OR || 
-            if (cardValue > 10 || cardSuit == 3) // ONE conditions must be true for the statement to be true. 
-            {
+            if (cardValue > 10 || cardSuit == 3) { // ONE conditions must be true for the statement to be true. 
                 Console.WriteLine("You have a face card or a Hearts.");
             }
-            else
-            {
+            else {
                 Console.WriteLine("You do not have a face card or a Hearts.");
             }
             */ 
 
             // Pause the video, use one of the logical operators to fix the if - else if - else block that prints the card value. 
-            if (cardValue > 14)
-            {
+            if (cardValue > 14) {
                 Console.WriteLine("Your card value is greater than 14, something wrong has occurred.  Please try again.");                      
             }
-            else if (cardValue == 14)
-            {
+            else if (cardValue == 14) {
                 Console.WriteLine("You have a card that is an Ace card.");                      
             }
-            else if (cardValue == 13)
-            {
+            else if (cardValue == 13) {
                 Console.WriteLine("You have a card that is a King card.");                      
             }
-            else if (cardValue == 12)
-            {
+            else if (cardValue == 12) {
                 Console.WriteLine("You have a card that is a Queen card.");                      
             }
-            else if (cardValue == 11)
-            {
+            else if (cardValue == 11) {
                 Console.WriteLine("You have a card that is a Jack card.");                      
             }
-            else if (cardValue < 11 && cardValue > 1) // If the card value is less than 11 and greater than 1.  
-            {
+            else if (cardValue < 11 && cardValue > 1) { // If the card value is less than 11 and greater than 1.  
                 Console.WriteLine("You have a card that is a 2-10.");                      
             }
-            else
-            {
+            else {
                 Console.WriteLine("Your card value was not saved correctly.  Please try again.");                      
             }
 
